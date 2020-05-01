ce828f0d66aecca8980bb36ead0007213d0a1768
Jacob Navia <jacob@jacob.remcomp.fr>
2020-05-01 22:55:32 +0200

    Added several versions contributed by Ben Bacarisse, and others

--- strrepl.c
+++ strrepl.c
@@ -1,17 +1,74 @@
+/* STRing REPLace, several programs that substitute one word for another in a buffer 
+All timings were done with a file of 4 351 187 bytes containing the text of the bible
+(bible.txt).
+The machine used for the tests is 
+jacob@rock64:~/strrepl$ uname -a
+Linux rock64 4.4.152-rockchip64 #1 SMP Sun Aug 26 14:40:54 CEST 2018 aarch64 GNU/Linux
+
+The gcc used is
+jacob@rock64:~/strrepl$ gcc -v
+Using built-in specs.
+COLLECT_GCC=gcc
+COLLECT_LTO_WRAPPER=/usr/lib/gcc/aarch64-linux-gnu/6/lto-wrapper
+Target: aarch64-linux-gnu
+gcc version 6.3.0 20170516 (Debian 6.3.0-18+deb9u1) 
+*/
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#if 1
+#define Strlen strlen
+#else
+/*
+This version of strlen is one of the fastest, published by D. E. Knuth.
+If your version of strlen is slow, use this one.
+*/
+#define H 0x8080808080808080ULL
+#define L 0x0101010101010101ULL
+static size_t Strlen(const char *s)
+{
+	unsigned long long t;
+	const char *save = s;
+	
+	while (1) {
+		// This supposes that the input string is aligned
+		// or that the machine doesn't trap when reading
+		// a 8 byte integer at a random position like the
+		// x86
+		t = *(unsigned long long *)s;
+		if (H & (t - L) & ~t)
+			break;
+		s += sizeof(long long);
+	}
+	// This loop will be executed at most 7 times
+	while (*s) {
+		s++;
+	}
+	return s - save;
+}
+#endif
+#if 0
+/*
+This version was contributed by jacob navia. It stores into a flexible array all 
+the hits, and uses that information later for the replacement.
+jacob@rock64:~/strrepl$ time ./strrepl-c bible.txt lived LIVEDFOREVER kk
+real	0m2.217s
+user	0m2.170s
+sys	0m0.045s
+*/
 #define CHUNKSIZE 100
 int strrepl(char *InputString,char *StringToFind,char *StringToReplace,char *output)
 {
 	char *offset = NULL, *CurrentPointer,**TableOfHits=NULL,*last;
 	size_t insertlen,capacity,tableIdx;
-	size_t findlen = strlen(StringToFind);
+	size_t findlen = Strlen(StringToFind);
 	size_t result,delta,total_length;
 	int FirstChar,i;
 
 	if (findlen == 0) return 0;
 	if (StringToReplace)
-		insertlen = strlen(StringToReplace);
+		insertlen = Strlen(StringToReplace);
 	else
 		insertlen = 0;
 	if (output) {
@@ -19,7 +76,7 @@ int strrepl(char *InputString,char *StringToFind,char *StringToReplace,char *out
 		if (output != InputString) *output = 0;
 	}
 	else {
-		result = strlen(InputString)+1;
+		result = Strlen(InputString)+1;
 		if (insertlen == findlen) return result;
 	}
 	FirstChar = *StringToFind;
@@ -98,8 +155,121 @@ int strrepl(char *InputString,char *StringToFind,char *StringToReplace,char *out
 	output[total_length]=0;
 	return result;
 }
+#elif 0
+/* 
+This version was contributed by Ben Bacarisse. It merges the two passes of the above version into one. In this machine
+it suffers from the bad performance of strstr.
+jacob@rock64:~/strrepl$ time ./strrepl-c bible.txt lived LIVEDFOREVER kk
+real	0m3.283s
+user	0m3.236s
+sys	0m0.045s
+*/
+int strrepl(const char *in, const char *pat, const char *rep, char *out)
+{
+     int patlen = Strlen(pat), replen = Strlen(rep), len = 0;
+     const char *src = in, *next;
+     while (next = strstr(src, pat)) {
+          if (out) memcpy(out + len, src, next - src);
+          len += next - src;
+          if (out) memcpy(out + len, rep, replen);
+          len += replen;
+          src = next + patlen;
+     }
+     int rest = Strlen(src);
+     if (out) memcpy(out + len, src, rest + 1);
+     return len + rest;
+} 
+#elif 1
+/* This version is the version of Mr Bacarisse modified to be more performant in a linux
+ARM 64 system. It replaces strstr with strncmp and makes some changes to improve
+performance in this system. This version is the fastest in this system.
+jacob@rock64:~/strrepl$ time ./strrepl-c bible.txt lived LIVEDFOREVER kk
+real	0m1.778s
+user	0m1.731s
+sys	0m0.047s
+*/
+int strrepl(const char *in, const char *pat, const char *rep, char *out)
+{
+     int patlen = Strlen(pat), replen = Strlen(rep), len = 0;
+	int FirstChar = *pat;
+     const char *src = in, *next;
 
-#include <stdio.h>
+	if (patlen == 0) return 0;
+	if (out) {
+	     for (next = src; *next;next++) {
+			if (FirstChar == *next && strncmp(next,pat,patlen) == 0) {
+	          memcpy(out + len, src, next - src);
+	          len += next - src;
+	          memcpy(out + len, rep, replen);
+	          len += replen;
+	          src = next = next + patlen;
+			}
+	     }
+	}
+    else {
+	     for (next = src; *next;next++) {
+				if (FirstChar == *next && strncmp(next,pat,patlen) == 0) {
+	          	len++;
+	          	next += patlen;
+			}
+	     }
+		 return (next - src) + (replen-patlen)*len;
+	}
+     int rest = next-src;
+     memcpy(out + len, src, rest + 1);
+     return len + rest;
+}
+#endif
+/*
+This version was contributed by "jak" It is slower than the others and modifies the interface,
+so I gave it another name
+*/
+int strrepl_jak(const char *in, size_t inSize, const char *pat, const char *rep, char *out)
+{
+    char *inEnd = (char *)in + inSize,
+         *inWalk, *p,
+         *patEnd = (char *)pat + (strlen(pat) - 1),
+         *repEnd = (char *)rep + (strlen(rep)),
+         *patWalk;
+    int  patCnt = 0;
+
+    patWalk = (char *)pat;
+    for(inWalk = (char *)in; inWalk < inEnd;)
+    {
+        if(*inWalk == *patWalk)
+        {
+            if(patWalk++ == patEnd)
+            {
+                patCnt++;
+                for(p = (char *)rep; out && p < repEnd; p++)
+                    *out++ = *p;
+                patWalk = (char *)pat;
+            }
+            inWalk++;
+        }
+        else
+        {
+            if(patWalk != pat)
+            {
+                for(p = (char *)pat; out && p < patWalk; p++)
+                    *out++ = *p;
+                patWalk = (char *)pat;
+            }
+            else
+            {
+                if(out)
+                    *out++ = *inWalk;
+                inWalk++;
+            }
+        }
+    }
+    return inSize + ((strlen(rep) - strlen(pat)) * patCnt);
+}
+
+/*
+Driver for the tests.
+*/
+
 int main(int argc,char *argv[])
 {
 	char *buffer,*tmp;
